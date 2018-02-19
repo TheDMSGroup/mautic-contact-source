40bbe3f55c1ce7e6524d2322cca53953bdb00e8e
heathdutton <hdutton@thedmsgrp.com>
2018-02-19 09:24:31 -0500

    Persist contacts to cache.

--- Controller/Api/ApiController.php
+++ Controller/Api/ApiController.php
@@ -13,18 +13,22 @@ namespace MauticPlugin\MauticContactServerBundle\Controller\Api;
 
 use FOS\RestBundle\Util\Codes;
 use Mautic\ApiBundle\Controller\CommonApiController;
+use Mautic\CampaignBundle\Entity\Campaign;
+use Mautic\CampaignBundle\Model\CampaignModel;
 use Mautic\CoreBundle\Helper\InputHelper;
+use Mautic\LeadBundle\Entity\Lead as Contact;
+use MauticPlugin\MauticContactServerBundle\Entity\ContactServer;
+use MauticPlugin\MauticContactServerBundle\Entity\Stat;
+use MauticPlugin\MauticContactServerBundle\Exception\ContactServerException;
 use MauticPlugin\MauticContactServerBundle\Model\Cache;
 use MauticPlugin\MauticContactServerBundle\Model\CampaignSettings;
 use MauticPlugin\MauticContactServerBundle\Model\ContactModel;
 use Symfony\Component\HttpFoundation\Request;
-use MauticPlugin\MauticContactServerBundle\Entity\Stat;
-use Mautic\LeadBundle\Entity\Lead as Contact;
-use Mautic\LeadBundle\Model\ListModel as LeadListModel;
-use MauticPlugin\MauticContactServerBundle\Exception\ContactServerException;
 
 /**
  * Class ContactServerApiController.
+ *
+ * @todo - This controller now contains too much business logic. Refactor offloading the logic to a model.
  */
 class ApiController extends CommonApiController
 {
@@ -35,6 +39,12 @@ class ApiController extends CommonApiController
     /** @var ContactModel */
     protected $contactModel;
 
+    /** @var ContactServer */
+    protected $contactServer;
+
+    /** @var Cache */
+    protected $cacheModel;
+
     /** @var boolean */
     protected $valid;
 
@@ -43,6 +53,18 @@ class ApiController extends CommonApiController
 
     /** @var array */
     protected $errors;
+
+    /** @var boolean */
+    protected $realTime;
+
+    /** @var boolean */
+    protected $scrub;
+
+    /** @var CampaignModel */
+    protected $campaignModel;
+
+    /** @var Campaign */
+    protected $campaign;
 
     /**
      * Primary API endpoint for servers to post contacts.
@@ -118,8 +140,8 @@ class ApiController extends CommonApiController
 
             // Check Server existence and published status.
             $serverModel = $this->get('mautic.contactserver.model.contactserver');
-            $contactServer = $serverModel->getEntity($serverId);
-            if (!$contactServer) {
+            $this->contactServer = $serverModel->getEntity($serverId);
+            if (!$this->contactServer) {
                 throw new ContactServerException(
                     'The serverId specified does not exist.',
                     0,
@@ -127,7 +149,7 @@ class ApiController extends CommonApiController
                     Stat::TYPE_INVALID,
                     'serverId'
                 );
-            } elseif ($contactServer->getIsPublished() === false) {
+            } elseif ($this->contactServer->getIsPublished() === false) {
                 throw new ContactServerException(
                     'The serverId specified has been unpublished (deactivated).',
                     0,
@@ -138,7 +160,7 @@ class ApiController extends CommonApiController
             }
 
             // Check authentication token against the server.
-            if ($token !== $contactServer->getToken()) {
+            if ($token !== $this->contactServer->getToken()) {
                 throw new ContactServerException(
                     'The token specified is invalid.', 0, null, Stat::TYPE_INVALID, 'token'
                 );
@@ -147,7 +169,7 @@ class ApiController extends CommonApiController
             // Check that the campaign is in the whitelist for this server.
             /** @var CampaignSettings $campaignSettingsModel */
             $campaignSettingsModel = $this->get('mautic.contactserver.model.campaign_settings');
-            $campaignSettingsModel->setContactServer($contactServer);
+            $campaignSettingsModel->setContactServer($this->contactServer);
             $campaignSettings = $campaignSettingsModel->getCampaignSettingsById($campaignId);
             // @todo - Support or thwart multiple copies of the same campaign, should it occur. In the meantime...
             $campaignSettings = reset($campaignSettings);
@@ -160,11 +182,12 @@ class ApiController extends CommonApiController
                     'campaignId'
                 );
             }
+            $this->realTime = (boolean)isset($campaignSettings->realTime) && $campaignSettings->realTime;
 
             // Check Campaign existence and published status.
-            $campaignModel = $this->get('mautic.campaign.model.campaign');
-            $campaign = $campaignModel->getEntity($campaignId);
-            if (!$campaign) {
+            /** @var Campaign $campaign */
+            $this->campaign = $this->getCampaignModel()->getEntity($campaignId);
+            if (!$this->campaign) {
                 throw new ContactServerException(
                     'The campaignId specified does not exist.',
                     0,
@@ -172,7 +195,7 @@ class ApiController extends CommonApiController
                     Stat::TYPE_INVALID,
                     'campaignId'
                 );
-            } elseif ($campaign->getIsPublished() === false) {
+            } elseif ($this->campaign->getIsPublished() === false) {
                 throw new ContactServerException(
                     'The campaignId specified has been unpublished (deactivated).',
                     0,
@@ -186,7 +209,7 @@ class ApiController extends CommonApiController
             $this->contact = $this->import($fieldData);
             if (!$this->contact) {
                 throw new ContactServerException(
-                    'Not enough valid data to create a contact.',
+                    'Not enough valid data was provided to create a contact.',
                     0,
                     null,
                     Stat::TYPE_INVALID
@@ -198,34 +221,48 @@ class ApiController extends CommonApiController
             // (This would best be done by cron, and cached somewhere as a list like campaign_required_fields)
             // (presuppose the overridden fields, if any)
 
-            // @todo - Evaluate Server+Campaign limits using the Cache
-            // /** @var Cache $cacheModel */
-            // $cacheModel = $this->get('mautic.contactserver.model.cache');
-            // $cacheModel->setContactServer($contactServer);
+            // @todo - Evaluate Server+Campaign limits using the Cache (method doesn't yet exist for either contact/client).
+            // $this->getCacheModel()->evaluateLimits();
 
-            // @todo - Evaluate Server duplicates.
+            // @todo - Evaluate Server duplicates against the cache. This is different than contact duplicates,
+            // as we only care about duplicates within the server. It is unsustainable to check against all contacts
+            // ever received by Mautic, so we only check for duplicates received by this server within a time frame.
 
-            // @todo - Evaluate scrub rate, and if scrubbed return negative status.
+            // @todo - Evaluate scrub rate, and if scrubbed we'll return negative status.
+            // @todo - Apply values(and scrub) to the contact for the mautic-contact-ledger plugin, if available.
+            $this->scrub = false;
+            if ($this->scrub) {
+                $this->status = Stat::TYPE_SCRUB;
+            }
 
             // Save the new contact.
             $this->getContactModel()->saveEntity($this->contact);
-            $this->status = Stat::TYPE_QUEUED;
+            $this->status = Stat::TYPE_SAVED;
             $data = $this->contact->getUpdatedFields();
             $data['id'] = $this->contact->getId();
             $this->valid = true;
 
-            // Add the contact to the campaign specified.
-            /** @var LeadListModel $leadListModel */
-            $leadListModel = $this->get('mautic.lead.model.list');
-            $leadListModel->addLead($this->contact, [$campaignId], false, false, -1);
+            // @todo - Optionally allow a segment to be targeted instead of a campaign in the future? No problem...
+            // /** @var \Mautic\LeadBundle\Model\ListModel $leadListModel */
+            // $leadListModel = $this->get('mautic.lead.model.list');
+            // $leadListModel->addLead($this->contact, [LeadList $list], true, !$this->realTime, -1);
 
-            // @todo - Create cache entry if all was successful.
+            // Add the contact directly to the campaign without duplicate checking.
+            $this->getCampaignModel()->addLeads($this->campaign, [$this->contact], false, true, -1);
+            $this->status = Stat::TYPE_QUEUED;
 
-            // @todo - Async: Accept the contact by return status.
+            // Create cache entry if all was successful for duplicate checking and limits.
+            $this->getCacheModel()->create();
 
-            // @todo - Sync: If this Server+Campaign is set to synchronous (and wasn't scrubbed), push the contact through the campaign now.
+            // @todo - Async (not real time): Accept the contact by return status.
+            if (!$this->realTime && !$this->scrub) {
+                $this->status = Stat::TYPE_ACCEPT;
+            }
 
-            // @todo - Sync: Evaluate the result of the campaign workflow and return status.
+            // @todo - Sync (real time): If this Server+Campaign is set to synchronous (and wasn't scrub), push the contact through the campaign now.
+            // if ($this->realTime && !$this->scrub) {
+            // @todo - Sync (real time): Evaluate the result of the campaign workflow and return status.
+            // }
 
         } catch (\Exception $e) {
             $field = null;
@@ -261,6 +298,19 @@ class ApiController extends CommonApiController
         $view->setFormat('json');
 
         return $this->handleView($view);
+    }
+
+    /**
+     * @return CampaignModel
+     */
+    private function getCampaignModel()
+    {
+        if (!$this->campaignModel) {
+            /** @var CampaignModel */
+            $this->campaignModel = $this->get('mautic.campaign.model.campaign');
+        }
+
+        return $this->campaignModel;
     }
 
     /**
@@ -313,6 +363,23 @@ class ApiController extends CommonApiController
 
         return $this->contactModel;
     }
+
+    /**
+     * @return Cache
+     * @throws \Exception
+     */
+    private function getCacheModel()
+    {
+        if (!$this->cacheModel) {
+            /** @var \MauticPlugin\MauticContactServerBundle\Model\Cache $cacheModel */
+            $this->cacheModel = $this->get('mautic.contactserver.model.cache');
+            $this->cacheModel->setContact($this->contact);
+            $this->cacheModel->setContactServer($this->contactServer);
+        }
+
+        return $this->cacheModel;
+    }
+
 
     // @todo - method to get a single request field and fall back to alternative names/locations.
 }
