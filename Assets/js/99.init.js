588bf042072a76cff7d631c139d628a4ebc04509
heathdutton <hdutton@thedmsgrp.com>
2018-03-06 15:07:02 -0500

    Widen Campaign view when possible.

--- Assets/js/99.init.js
+++ Assets/js/99.init.js
@@ -3,5 +3,25 @@ Mautic.contactsourceOnLoad = function () {
     mQuery(document).ready(function () {
         Mautic.contactsourceDocumentation();
         Mautic.contactsourceCampaigns();
+
+        // Hide the right column when Campaigns tab is open to give more room
+        // for table entry.
+        var activeTab = '#details';
+        mQuery('.contactsource-tab').click(function () {
+            var thisTab = mQuery(this).attr('href');
+            if (thisTab !== activeTab) {
+                activeTab = thisTab;
+                if (activeTab === '#campaigns') {
+                    // Expanded view.
+                    mQuery('.contactsource-left').addClass('col-md-12').removeClass('col-md-9');
+                    mQuery('.contactsource-right').addClass('hide');
+                }
+                else {
+                    // Standard view.
+                    mQuery('.contactsource-left').removeClass('col-md-12').addClass('col-md-9');
+                    mQuery('.contactsource-right').removeClass('hide');
+                }
+            }
+        });
     });
 };
