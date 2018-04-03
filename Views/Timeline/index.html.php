9bb8037f4201fd459ab0df8c554cd43435141e09
scottshipman <k.scott.shipman@gmail.com>
2018-04-03 10:37:06 -0400

    [ENG-189] stub out js and templates

--- Views/Timeline/index.html.php
+++ Views/Timeline/index.html.php
@@ -9,62 +9,57 @@
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 ?>
+<?php
+$orderBy          = isset($events['filters']['order']) && !empty($events['filters']['order'][0]) ? $events['filters']['order'][0] : 'date_added';
+$orderByDirection = isset($events['filters']['order']) && !empty($events['filters']['order'][1]) ? $events['filters']['order'][1] : 'DESC';
+$page             = isset($events['page']) && !empty($events['page']) ? $events['page'] : 1;
+
+?>
 
 <!-- filter form -->
+<h4><?php echo $view['translator']->trans('mautic.contactsource.search.header'); ?></h4>
 <form method="post" action="<?php echo $view['router']->path(
     'mautic_contactsource_timeline_action',
     ['contactSourceId' => $contactSource->getId()]
 ); ?>" class="panel" id="timeline-filters">
-    <div class="form-control-icon pa-xs">
-        <input type="text" class="form-control bdr-w-0" name="search" id="search"
-               placeholder="<?php echo $view['translator']->trans('mautic.core.search.placeholder'); ?>"
-               value="<?php echo $events['filters']['search']; ?>">
-        <span class="the-icon fa fa-search text-muted mt-xs"></span>
-    </div>
-    <?php if (isset($events['types']) && is_array($events['types'])) : ?>
-        <div class="history-search panel-footer text-muted">
-            <div class="col-sm-5">
-                <select name="includeEvents[]" multiple="multiple" class="form-control bdr-w-0"
-                        data-placeholder="<?php echo $view['translator']->trans(
-                            'mautic.contactsource.events.filter.include.placeholder'
-                        ); ?>">
-                    <?php foreach ($events['types'] as $typeKey => $typeName) : ?>
-                        <option value="<?php echo $typeKey; ?>"<?php echo isset($events['filters']['includeEvents']) && in_array(
-                            $typeKey,
-                            $events['filters']['includeEvents']
-                        ) ? ' selected' : ''; ?> >
-                            <?php echo $typeName; ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
+    <div class="col-xs-8 col-lg-10 va-m form-inline">
+        <div class="input-group col-xs-8">
+            <input type="text" class="form-control bdr-w-1 search tt-input" name="search" id="search"
+                   placeholder="<?php echo $view['translator']->trans('mautic.contactsource.search.placeholder'); ?>"
+                   value="<?php echo $events['filters']['search']; ?>">
+
+            <div class="input-group-btn">
+                <button type="submit" id="contactSourceTimelineFilterApply" name="contactSourceTimelineFilterApply"
+                        class="btn btn-default btn-search btn-nospin">
+                    <i class="the-icon fa fa-search fa-fw"></i>
+                </button>
             </div>
-            <div class="col-sm-5">
-                <select name="excludeEvents[]" multiple="multiple" class="form-control bdr-w-0"
-                        data-placeholder="<?php echo $view['translator']->trans(
-                            'mautic.contactsource.events.filter.exclude.placeholder'
-                        ); ?>">
-                    <?php foreach ($events['types'] as $typeKey => $typeName) : ?>
-                        <option value="<?php echo $typeKey; ?>"<?php echo isset($events['filters']['excludeEvents']) && in_array(
-                            $typeKey,
-                            $events['filters']['excludeEvents']
-                        ) ? ' selected' : ''; ?> >
-                            <?php echo $typeName; ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
+
             <?php /* @todo - export action. Doesn't yet have a router/controller config.
              * <div class="col-sm-2">
-             * <a class="btn btn-default btn-block" href="<?php echo $view['router']->generate('mautic_contactsource_timeline_export_action', ['contactSourceId' => $contactSource->getId()]); ?>" data-toggle="download">
+             * <a class="btn btn-default btn-block" href="<?php echo
+             * $view['router']->generate('mautic_contactclient_timeline_export_action', ['contactClientId' =>
+             * $contactClient->getId()]); ?>" data-toggle="download">
              * <span>
-             * <i class="fa fa-download"></i> <span class="hidden-xs hidden-sm"><?php echo $view['translator']->trans('mautic.core.export'); ?></span>
+             * <i class="fa fa-download"></i> <span class="hidden-xs hidden-sm"><?php echo
+             * $view['translator']->trans('mautic.core.export'); ?></span>
              * </span>
              * </a>
              * </div>*/ ?>
+            <div class="input-group-btn" style="width:auto;font-size:1em;padding-left:4px;"
+            ">
+                <input id="include-logs" type="checkbox"
+                       title="Apply search term to verbose logs - may cause unexpected results." name="logs"
+                       class="bdr-w-0">
+                <label style="padding:4px;" for="include-logs">Apply search term to verbose logs.</label>
+            </div>
         </div>
-    <?php endif; ?>
+    </div>
 
-    <input type="hidden" name="leadId" id="leadId" value="<?php echo $contactSource->getId(); ?>"/>
+
+    <input type="hidden" name="contactSourceId" id="contactSourceId" value="<?php echo $contactSource->getId(); ?>"/>
+    <input type="hidden" name="orderBy" id="orderBy" value="<?php echo $orderBy; ?>:<?php echo $orderByDirection; ?>"/>
+    <input type="hidden" name="page" id="page" value="<?php echo $page; ?>"/>
 </form>
 
 <script>
@@ -75,3 +70,41 @@
 <div id="timeline-table">
     <?php $view['slots']->output('_content'); ?>
 </div>
+<script>
+    mQuery(function () {
+        var filterForm = mQuery('#timeline-filters');
+        var dateFrom = document.createElement('input');
+        dateFrom.type = 'hidden';
+        dateFrom.name = 'dateFrom';
+        dateFrom.value = mQuery('#chartfilter_date_from').val();
+
+        var dateTo = document.createElement('input');
+        dateTo.type = 'hidden';
+        dateTo.name = 'dateTo';
+        dateTo.value = mQuery('#chartfilter_date_to').val();
+
+        filterForm.append(dateFrom);
+        filterForm.append(dateTo);
+
+        filterForm.submit(function (event) {
+            event.preventDefault(); // Prevent the form from submitting via the browser
+            var form = $(this);
+            mQuery.ajax({
+                type: form.attr('method'),
+                url: mauticAjaxUrl,
+                data: {
+                    action: 'plugin:mauticContactSource:ajaxTimeline',
+                    filters: form.serializeArray(),
+                },
+            }).done(function (data) {
+                mQuery('div#timeline-table').html(data);
+                if (mQuery('#contactsource-timeline').length) {
+                    Mautic.contactsourceTimelineOnLoad();
+                }
+            }).fail(function (data) {
+                // Optionally alert the user of an error here...
+                alert('Ooops! Something went wrong');
+            });
+        });
+    });
+</script>
