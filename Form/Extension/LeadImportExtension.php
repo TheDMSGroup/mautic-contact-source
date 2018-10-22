27a29dbdd7244158e0e2d3df1a1a2d3e201d5467
scottshipman <k.scott.shipman@gmail.com>
2018-10-22 11:56:57 -0400

    [ENG-616] auto poulate the source AND campaign via GET params as a shortcut

--- Form/Extension/LeadImportExtension.php
+++ Form/Extension/LeadImportExtension.php
@@ -50,57 +50,79 @@ class LeadImportExtension extends AbstractTypeExtension
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         if (isset($_GET['source'])) {
-            $sourceName = $this->em->getRepository('MauticContactSourceBundle:ContactSource')->find($_GET['source'])->getName();
+            $sourceEntity      = $this->em->getRepository('MauticContactSourceBundle:ContactSource')->find(
+                $_GET['source']
+            );
+            $sourceEntityLabel = $sourceEntity->getName();
+        } else {
+            $sourceEntityLabel = 'Select A Source';
+            $sourceEntity      = [];
         }
-        $emptySource = isset($_GET['source']) ? $sourceName : 'Select A Source';
+
+        if (isset($_GET['campaign'])) {
+            $campaignEntity      = $this->em->getRepository('MauticCampaignBundle:Campaign')->find($_GET['campaign']);
+            $campaignEntityLabel = $campaignEntity->getName();
+        } else {
+            $campaignEntityLabel = 'Select A Source';
+            $campaignEntity      = [];
+        }
 
         $builder->add(
-          'source',
-          EntityType::class,
-          [
-              'class'        => 'MauticContactSourceBundle:ContactSource',
-              'empty_value'  => $emptySource,
-              'choice_label' => function ($source) {
-                  return $source->getName();
-              },
-          ]
+            'source',
+            EntityType::class,
+            [
+                'class'        => 'MauticContactSourceBundle:ContactSource',
+                'data'         => $sourceEntity,
+                'placeholder'  => $sourceEntityLabel,
+                'choice_label' => function ($sourceEntity) {
+                    return $sourceEntity->getName();
+                },
+            ]
         );
 
         $builder->get('source')
-
-            ->addModelTransformer(new CallbackTransformer(
-                function ($source) {
-                    // transform the ID back to an Object
-                    return $source ? $this->em->getRepository('MauticContactSourceBundle:ContactSource')->find($source) : null;
-                },
-                function ($source) {
-                    // transform the object to a ID
-                    return $source ? $source->getID() : null;
-                }
-            ));
+            ->addModelTransformer(
+                new CallbackTransformer(
+                    function ($source) {
+                        // transform the ID back to an Object
+                        return $source ? $this->em->getRepository('MauticContactSourceBundle:ContactSource')->find(
+                            $source
+                        ) : null;
+                    },
+                    function ($source) {
+                        // transform the object to a ID
+                        return $source ? $source->getID() : null;
+                    }
+                )
+            );
 
         $builder->add(
             'campaign',
             EntityType::class,
             [
                 'class'        => 'MauticCampaignBundle:Campaign',
-                'empty_value'  => 'Select A Campaign',
-                'choice_label' => function ($campaign) {
-                    return $campaign->getName();
+                'data'         => $campaignEntity,
+                'empty_value'  => $campaignEntityLabel,
+                'choice_label' => function ($campaignEntity) {
+                    return $campaignEntity->getName();
                 },
             ]
         );
 
         $builder->get('campaign')
-            ->addModelTransformer(new CallbackTransformer(
-                function ($campaign) {
-                    // transform the ID back to an Object
-                    return $campaign ? $this->em->getRepository('MauticCampaignBundle:Campaign')->find($campaign) : null;
-                },
-                function ($campaign) {
-                    // transform the object to a ID
-                    return $campaign ? $campaign->getID() : null;
-                }
-            ));
+            ->addModelTransformer(
+                new CallbackTransformer(
+                    function ($campaign) {
+                        // transform the ID back to an Object
+                        return $campaign ? $this->em->getRepository('MauticCampaignBundle:Campaign')->find(
+                            $campaign
+                        ) : null;
+                    },
+                    function ($campaign) {
+                        // transform the object to a ID
+                        return $campaign ? $campaign->getID() : null;
+                    }
+                )
+            );
     }
 }
