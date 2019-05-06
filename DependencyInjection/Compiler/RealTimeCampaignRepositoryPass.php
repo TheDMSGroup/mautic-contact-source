425eacfb402f84c2647347bd733a38fb359ae227
Dean Martin <contact@deanmartin.pro>
2019-05-06 12:20:21 -0400

    [ENG-892] Formatting.

--- DependencyInjection/Compiler/RealTimeCampaignRepositoryPass.php
+++ DependencyInjection/Compiler/RealTimeCampaignRepositoryPass.php
@@ -3,11 +3,9 @@
 namespace MauticPlugin\MauticContactSourceBundle\DependencyInjection\Compiler;
 
 use MauticPlugin\MauticContactSourceBundle\Entity\RealTimeCampaignRepository;
-use Mautic\CampaignBundle\Entity\Campaign;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Serializer\Mapping\ClassMetadata;
 
 class RealTimeCampaignRepositoryPass implements CompilerPassInterface
 {
@@ -19,7 +17,7 @@ class RealTimeCampaignRepositoryPass implements CompilerPassInterface
         $container->getDefinition('mautic.campaign.repository.campaign')
                                 ->setFactory(null)
                                 ->setArguments([
-                                    new Reference('doctrine.orm.entity_manager')
+                                    new Reference('doctrine.orm.entity_manager'),
                                 ])
                                 ->setClass(RealTimeCampaignRepository::class);
     }
