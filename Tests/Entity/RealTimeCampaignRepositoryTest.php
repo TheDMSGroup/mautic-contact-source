48f0a6c3a820f9d97160cace1956c06823884e00
Dean Martin <contact@deanmartin.pro>
2019-05-07 12:43:55 -0400

    [ENG-892] Added a basic test for RealTimeCampaignRepository, but having trouble with changing the database config for test env. This is fine for now

--- Tests/Entity/RealTimeCampaignRepositoryTest.php
+++ Tests/Entity/RealTimeCampaignRepositoryTest.php
@@ -2,15 +2,40 @@
 
 namespace MauticPlugin\MauticContactSourceBundle\Tests\Entity;
 
-use MauticPlugin\MauticContactSourceBundle\Tests\RepositoryTest;
-use Mautic\CampaignBundle\Entity\Campaign;
+use Mautic\CampaignBundle\Executioner\ContactFinder\Limiter\ContactLimiter;
+use Mautic\CoreBundle\Test\AbstractMauticTestCase;
+use MauticPlugin\MauticContactSourceBundle\Entity\RealTimeCampaignRepository;
 
-class RealTimeCampaignRepositoryTest extends RepositoryTest
+class RealTimeCampaignRepositoryTest extends AbstractMauticTestCase
 {
+    // Having Datbase connection issues in test config,
+
     /** @test */
-    function it_pulls_contact_ids_by_batches()
+    /* public function it_doesnt_use_real_time_if_contact_is_not_defined() */
+    /* { */
+    /*     $this->applyMigrations(); */
+    /*     $repo    = new RealTimeCampaignRepository($this->em); */
+
+    /*     $contactIds = [1, 2, 3, 4, 5]; */
+    /*     $limiter    = new ContactLimiter(1, null, null, null, $contactIds, null, null, 5); */
+
+    /*     $ids     = $repo->getPendingContactIds(1, $limiter); */
+    /*     dump($ids); */
+    /* } */
+
+    /** @test */
+    public function it_pulls_contact_ids_by_batches()
     {
-        $repo = $this->em->getRepository(Campaign::class); 	
-        dump($repo);
-    } 
+        define('MAUTIC_PLUGIN_CONTACT_SOURCE_REALTIME', true);
+        $repo    = new RealTimeCampaignRepository($this->em);
+
+        $contactIds = [1, 2, 3, 4, 5];
+        $limiter    = new ContactLimiter(1, null, null, null, $contactIds, null, null, 5);
+
+        $ids     = $repo->getPendingContactIds(1, $limiter);
+        $this->assertEquals($contactIds, $ids);
+
+        $ids     = $repo->getPendingContactIds(1, $limiter);
+        $this->assertEmpty($ids);
+    }
 }
